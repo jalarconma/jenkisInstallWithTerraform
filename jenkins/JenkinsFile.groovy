e2289264c4fcacd79d2dbb5d07508ea38b945c56
Julian Alarcon - Technology <jalarcon@CX.COL>
2019-08-02 17:30:53 -0500

    change jkfile

--- jenkins/JenkinsFile.groovy
+++ jenkins/JenkinsFile.groovy
@@ -3,23 +3,39 @@
 pipeline {
     agent any
     stages {
-        stage('Build') {
+        stage('Prepare'){
             steps {
-                sh 'echo "Hello World, Im building"'
-                sh '''
-                    echo "Multiline shell steps works too"
-                    ls -lah
-                '''
+                 sh 'echo "Hello World, Im preparing"'
             }
         }
 
-        stage('Deploy') {
+        stage('Download from repository'){
             steps {
-                sh 'echo "Hello World, im deploying"'
-                sh '''
-                    echo "Multiline shell steps works too"
-                    ls -lah
-                '''
+                 sh 'echo "Hello World, Im pulling"'
+            }
+        }
+
+        stage('Build'){
+            steps {
+                 sh 'echo "Hello World, Im building"'
+            }
+        }
+
+        stage('Upload artifacts'){
+            steps {
+                 sh 'echo "Hello World, Im uploading"'
+            }
+        }
+
+        stage('Deploy'){
+            steps {
+                 sh 'echo "Hello World, Im deploying"'
+            }
+        }
+
+        stage('Test'){
+            steps {
+                 sh 'echo "Hello World, Im testing"'
             }
         }
     }
