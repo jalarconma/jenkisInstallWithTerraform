0242ceb1ce126fb2018c71850ce48e77f9e30926
Julian Alarcon - Technology <jalarcon@CX.COL>
2019-08-02 16:21:14 -0500

    change jkfile

--- jenkins/JenkinsFile.groovy
+++ jenkins/JenkinsFile.groovy
@@ -5,7 +5,17 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'echo "Hello World"'
+                sh 'echo "Hello World, Im building"'
+                sh '''
+                    echo "Multiline shell steps works too"
+                    ls -lah
+                '''
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                sh 'echo "Hello World, im deploying"'
                 sh '''
                     echo "Multiline shell steps works too"
                     ls -lah
